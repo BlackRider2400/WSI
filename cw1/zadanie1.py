86a956ed622ee16d94418bd53bedd937951d4c2a
BlackRider2400 <krzysztof@baralkiewicz.pl>
2024-10-16 22:51:04 +0200

    made basic functions for first exercise

--- cw1/zadanie1.py
+++ cw1/zadanie1.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 
-# Definicja przedmiotów
-m = np.array([8, 3, 5, 2])  # masa przedmiotów
-p = np.array([16, 8, 9, 6])  # wartość przedmiotów
-M = np.sum(m) / 2  # maksymalna masa plecaka
+def brute_force(list_mass, list_items_val, max_mass):
+    items = []
+    for i in range(len(list_mass)):
+        items.append((list_mass[i], list_items_val[i]))
 
-def brute_force():
     all_options = []
-    for i in range(len(m)):
+    for i in range(len(list_mass)**2):
         binary_format = bin(i)[2:]
+        binary_format = binary_format.rjust(len(list_mass), "0")
         all_options.append(binary_format)
 
     max_val = -1
@@ -17,19 +17,54 @@ def brute_force():
     best_option = ""
 
     for i in all_options:
-        sum = 0
+        sum_of_values = 0
         mass = 0
         for j in range(len(i)):
             if i[j] == "1":
-                sum += int(p[j])
-                mass += int(m[i])
-                if mass > M:
+                sum_of_values += int(list_items_val[j])
+                mass += int(list_mass[j])
+                if mass > max_mass:
                     break
 
-        if mass <= M and sum > max_val:
-            max_val = sum
+        if mass <= max_mass and sum_of_values > max_val:
+            max_val = sum_of_values
             best_mass = mass
             best_option = i
 
-brute_force()
+    out_put = []
+
+    for i in range(len(list_mass)):
+        if best_option[i] == "1":
+            out_put.append((list_mass[i], list_items_val[i]))
+
+    return out_put
+
+
+def heuristics(list_mass, list_items_val, max_mass):
+    items = []
+    for i in range(len(list_mass)):
+        items.append((list_mass[i], list_items_val[i]))
+
+    items.sort(key=lambda x: (x[1] / x[0]), reverse=True)
+
+    mass = 0
+
+    out_put = []
+
+    for i in items:
+        if i[0] + mass <= max_mass:
+            out_put.append(i)
+            mass += i[0]
+
+
+    return out_put
+
+
+if __name__ == '__main__':
+    m = [8, 3, 5, 2]
+    p = [16, 8, 9, 6]
+    M = np.sum(m) / 2
+
+    print(brute_force(m, p, M))
+    print(heuristics(m, p, M))
 
