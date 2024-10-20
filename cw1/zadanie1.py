d96ea924a3430d1cbb597f7ac7c7aa98126193b3
BlackRider2400 <krzysztof@baralkiewicz.pl>
2024-10-20 15:58:28 +0200

    almost done

--- cw1/zadanie1.py
+++ cw1/zadanie1.py
@@ -8,32 +8,28 @@ def brute_force(list_mass, list_items_val, max_mass):
     for i in range(len(list_mass)):
         items.append((list_mass[i], list_items_val[i]))
 
-    all_options = []
-    for i in range(2**(len(list_mass))):
-        binary_format = bin(i)[2:]
-        binary_format = binary_format.rjust(len(list_mass), "0")
-        all_options.append(binary_format)
-
     max_val = -1
     best_mass = -1
     best_option = ""
 
-    for i in all_options:
+    for i in range(2**(len(list_mass))):
+        binary_format = bin(i)[2:]
+        binary_format = binary_format.rjust(len(list_mass), "0")
+
         sum_of_values = 0
         mass = 0
-        for j in range(len(i)):
-            if i[j] == "1":
+        for j in range(len(binary_format)):
+            if binary_format[j] == "1":
                 sum_of_values += int(list_items_val[j])
                 mass += int(list_mass[j])
 
             if mass > max_mass:
                 break
 
-
         if mass <= max_mass and sum_of_values > max_val:
             max_val = sum_of_values
             best_mass = mass
-            best_option = i
+            best_option = binary_format
 
     out_put = []
 
@@ -61,7 +57,6 @@ def heuristics(list_mass, list_items_val, max_mass):
             out_put.append(i)
             mass += i[0]
             value += i[1]
-
 
     return out_put, mass, value
 
@@ -91,9 +86,11 @@ def test_data(list_mass, list_items_val):
         file.write("Heuristics time: " + "{0:02f}s".format(end_time_heuristics - start_time_heuristics))
         file.write("\n--------------------------------------\n")
 
+    print(f"Finished calculating for {len(list_mass)}.")
 
 
 if __name__ == '__main__':
+    max_int = 50
     m = [8, 3, 5, 2]
     p = [16, 8, 9, 6]
 
@@ -105,9 +102,72 @@ if __name__ == '__main__':
     m = []
     p = []
 
-    for i in range(100):
-        m.append(random.randint(1, 30))
-        p.append(random.randint(1, 30))
+    for i in range(10):
+        m.append(random.randint(1, max_int))
+        p.append(random.randint(1, max_int))
+
+    test_data(m, p)
+
+    m = []
+    p = []
+
+    for i in range(15):
+        m.append(random.randint(1, max_int))
+        p.append(random.randint(1, max_int))
+
+    test_data(m, p)
+
+    m = []
+    p = []
+
+    for i in range(20):
+        m.append(random.randint(1, max_int))
+        p.append(random.randint(1, max_int))
+
+    test_data(m, p)
+
+    m = []
+    p = []
+
+    for i in range(21):
+        m.append(random.randint(1, max_int))
+        p.append(random.randint(1, max_int))
+
+    test_data(m, p)
+
+    m = []
+    p = []
+
+    for i in range(22):
+        m.append(random.randint(1, max_int))
+        p.append(random.randint(1, max_int))
+
+    test_data(m, p)
+
+    m = []
+    p = []
+
+    for i in range(23):
+        m.append(random.randint(1, max_int))
+        p.append(random.randint(1, max_int))
+
+    test_data(m, p)
+
+    m = []
+    p = []
+
+    for i in range(24):
+        m.append(random.randint(1, max_int))
+        p.append(random.randint(1, max_int))
+
+    test_data(m, p)
+
+    m = []
+    p = []
+
+    for i in range(25):
+        m.append(random.randint(1, max_int))
+        p.append(random.randint(1, max_int))
 
     test_data(m, p)
 
